dc9ac69cd7a8a1387f535808baa220b87af92265
lhn <320534369@qq.com>
2023-12-10 23:01:35 +0800

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
